0c40257a7d96affdd1bc07183db84a7562d9f2d9
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-26 16:58:26 +0530

    added error check for email and password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response} from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-    res.json("Hello");
+router.post("/api/users/signup", [
+        body('email')
+        .isEmail()
+        .withMessage("Enter valid email"),
+        body("password")
+        .trim()
+        .isLength({min: 4, max:20})
+    ], 
+    (req:Request, res: Response) => {
+        const errors = validationResult(req);
+        
+        if(!errors.isEmpty()){
+            return res.status(400).send(errors.array());
+        }
+
+        res.send({});
 });
 
 export { router as signupRouter };
