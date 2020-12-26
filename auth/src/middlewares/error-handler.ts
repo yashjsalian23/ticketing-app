f1d3da2fc725e6c04616b0bf6b2ae80b2250319d
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-26 23:11:51 +0530

    added error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,11 +1,17 @@
-import { Response, Request, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
-err: Error, 
-res: Response, 
-req: Request, 
-next: NextFunction) => {
-    res.status(400).send({
-        message: err.message
-    })
-}
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }]
+  });
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -13,11 +15,11 @@ router.post("/api/users/signup", [
     ], 
     (req:Request, res: Response) => {
         const errors = validationResult(req);
-        
-        if(!errors.isEmpty()){
-            return res.status(400).send(errors.array());
-        }
 
+        if(!errors.isEmpty()){
+            throw new RequestValidationError(errors.array());
+        }
+        throw new DatabaseConnectionError();
         res.send({});
 });
 
