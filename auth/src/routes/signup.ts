da7c6284dcdb1e606196e7f696cd2794a2f56240
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-27 20:51:06 +0530

    added signup logic --basic

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -13,14 +13,24 @@ router.post("/api/users/signup", [
         .trim()
         .isLength({min: 4, max:20})
     ], 
-    (req:Request, res: Response) => {
+    async (req:Request, res: Response) =>  {
         const errors = validationResult(req);
 
         if(!errors.isEmpty()){
             throw new RequestValidationError(errors.array());
         }
-        throw new DatabaseConnectionError();
-        res.send({});
+
+        const { email, password } = req.body;
+        const existingEmail = await User.findOne({email});
+        if(existingEmail){
+            console.log("Email exists");
+            return res.send({});
+        }
+
+        const user = User.build({email, password});
+        await user.save();
+
+        res.status(201).send({user});
 });
 
 export { router as signupRouter };
