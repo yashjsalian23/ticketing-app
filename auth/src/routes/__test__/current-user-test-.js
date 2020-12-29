72cd6ccb0b437c51e768650053861886c5cea585
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 23:02:00 +0530

    added check for unsigned users

--- auth/src/routes/__test__/current-user-test-.js
+++ auth/src/routes/__test__/current-user-test-.js
@@ -2,8 +2,6 @@ import request from 'supertest'; //used for http reqs
 import { app } from '../../app';
 
 it('responds with details about current user', async () => {
-
-    
     const cookie = await global.signin()
 
     const response = await request(app)
@@ -13,4 +11,13 @@ it('responds with details about current user', async () => {
         expect(200)
 
     expect(response.body.currentUser.email).toEqual('test@test.com'); 
+});
+
+it('responds with null if user not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentUser')
+        .send().
+        expect(200)
+
+    expect(response.body.currentUser).toEqual(null); 
 })
