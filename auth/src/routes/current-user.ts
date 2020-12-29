7c66a2feb8971defe62584e781d94f2b1bb6d672
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 14:37:07 +0530

    added current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-    res.json("Hello");
+    if(!req.session || !req.session.jwt){
+        return res.send({currentUser: null});
+    }
+
+    try{
+        const payload = jwt.verify(
+            req.session.jwt, 
+            process.env.JWT_KEY!
+        );
+        res.send({currentUser: payload})
+    } catch(err){
+        return res.send({currentUser: null});
+    }
+   
 });
 
 export { router as currentUserRouter };
