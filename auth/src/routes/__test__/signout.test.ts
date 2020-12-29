12821ade29edd2a27ced404056a4b2d1cacbd16e
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 22:36:11 +0530

    added test for signout

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,20 @@
+import { response } from 'express';
+import request from 'supertest'; //used for http reqs
+import { app } from '../../app';
+
+it('clears cookie on signout', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(201)
+    
+    const response = await request(app)
+        .post('/api/users/signout')
+        .send({})
+        .expect(200)
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+})
