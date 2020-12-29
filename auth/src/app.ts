32379cdda1dc193d204be9a2a03f23e38de1d234
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 21:12:21 +0530

    added app.ts file

---
+++ auth/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import  { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy',true);  //tells express that req from ingress is secure
+app.use(json());
+
+//intializing cookie session
+app.use(cookieSession({
+  signed: false, //no encryption
+  secure: true //only from https
+}))
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", () => { throw new NotFoundError()});
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import  { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy',true);  //tells express that req from ingress is secure
-app.use(json());
-
-//intializing cookie session
-app.use(cookieSession({
-  signed: false, //no encryption
-  secure: true //only from https
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", () => { throw new NotFoundError()});
+import { app } from './app';
 
 const start = async () => {
   if(!process.env.JWT_KEY){
