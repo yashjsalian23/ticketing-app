c3d94bc1927edc99338a8c8c00c76e50f97e3555
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 21:52:21 +0530

    added signup test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from '../app';
 
 let mongo: any;
 beforeAll(async () => {
+    process.env.JWT_KEY = 'ejfkfww';
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
