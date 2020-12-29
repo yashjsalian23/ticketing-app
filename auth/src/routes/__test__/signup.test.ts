4dccab61e1fa32409135abd39c12338ec6949e05
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-29 22:01:33 +0530

    added more tests on signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,37 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201)
 });
+
+it('returns a 400 on invalid email or password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: "nkkjskd",
+        password: "password"
+    })
+    .expect(400)
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: "test@test.com",
+        password: "p"
+    })
+    .expect(400)
+});
+
+it('returns a 400 on empty email or password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: "test@test.com",
+    })
+    .expect(400)
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        password: "password",
+    })
+    .expect(400)
+});
