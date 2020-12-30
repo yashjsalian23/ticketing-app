3f2e3b7c7dce8984756ddac2e1ada4ddf04fa34e
yashjsalian23 <yashjsalian23@gmail.com>
2020-12-30 22:43:38 +0530

    completed signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,19 +1,27 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+
+import useRequest from '../../hooks/use-request';
 
 export default () => {
 
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+          email,
+          password
+        },
+        onSuccess: () => Router.push('/')
+      });
+    
 
     const formSubmitHandler = async event => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', {
-            email,
-            password
-        });
-        console.log(response);
+        await doRequest();
     }
 
     return (
@@ -32,6 +40,8 @@ export default () => {
                     required
                     value={password}
                     onChange={e => setPassword(e.target.value)} />
+
+                    {errors}
                 <button type="submit" className="btn btn-primary">Submit</button>
             </div>
         </form>
