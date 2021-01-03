fa352ceebb5ac0467c029f1bd3de024880759c45
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-03 22:09:22 +0530

    added header and signout logic

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,33 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css';  //required to add any global css in next
 import buildClient from '../api/build-client';
+import Header from '../components/Header';
 
-const AppComponent = ({ Component, pageProps }) => {
-    return (
-        <div>
-            <h1>Header</h1>
-            <Component {...pageProps} />
-        </div>
-    ) 
-}
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
 //global getInitalProps
 //context here is different from one in page (there are more info)
-AppComponent.getInitalProps = async(appContext) =>{
-    const { data } = await buildClient(appContext.ctx).get("/api/users/currentUser");
-
+AppComponent.getInitialProps = async appContext => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentUser');
     //global getInitalProps overrides page's one. So to execute page's one we have to extract the pageProps from the Component key
     let pageProps = {};
     //if check is because not all pages may have getInitalProps
-    if(appContext.Component.getInitalProps){
-        pageProps = await appContext.Component.getInitalProps(appContext.ctx);
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
     }
 
-    return {
-        pageProps,
-        ...data
-    }
-
-}
-
-
+  return {
+    pageProps,
+    ...data
+  };
+};
 
 export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,18 @@
 import buildClient from '../api/build-client';
 
-const LandingPage =  ({currentUser}) => {
-    let content = currentUser ? 'You are signed in' : 'You are not signed in';
-    return <h1>{content}</h1>
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
 
-}
-//this method will run in the server
-//req will be made from browser only when a page is redirected from another.For other cases (refresh, direct url, etc) req made from server
-LandingPage.getInitialProps = async (context) => {
-   const { data } = await buildClient(context).get("/api/users/currentUser");
-   return data;
-}
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentUser');
+
+  return data;
+};
 
 export default LandingPage;
