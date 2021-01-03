a690c692b6bc0e934f69ea0ed776be847611f75d
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-03 13:27:32 +0530

    added additional comments

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,6 +7,7 @@ const LandingPage =  ({currentUser}) => {
     
 }
 //this method will run in the server
+//req will be made from browser only when a page is redirected from another.For other cases (refresh, direct url, etc) req made from server
 LandingPage.getInitialProps = async () => {
     if(typeof window === 'undefined'){ //check is req is made in server or browser
         const { data } = await axios.get( //format: http://SERVICENAME.NAMESPACE.cluster.local/*
