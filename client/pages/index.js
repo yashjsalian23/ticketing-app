b24268aea54e3722af7a67ccb13e5bd80513e9d0
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-03 13:22:45 +0530

    added req for both server and browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,25 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage =  ({currentUser}) => {
+    console.log(currentUser);
+    // axios.get('/api/users/currentUser');
     return <h1>Landing Page</h1>
+    
 }
+
+LandingPage.getInitialProps = async () => {
+    if(typeof window === 'undefined'){
+        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',{
+            headers:{
+                Host:'ticketing.dev'
+            }
+        });
+        return data;
+    } else {
+        const { data } = await axios.get('/api/users/currentUser');
+        return data;
+    }
+    return {};
+}
+
+export default LandingPage;
