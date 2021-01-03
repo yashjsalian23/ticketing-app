65a9bc8577fa5de6954388fa8e7c2a3155901947
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-03 13:24:53 +0530

    added comments

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,16 +6,17 @@ const LandingPage =  ({currentUser}) => {
     return <h1>Landing Page</h1>
     
 }
-
+//this method will run in the server
 LandingPage.getInitialProps = async () => {
-    if(typeof window === 'undefined'){
-        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',{
+    if(typeof window === 'undefined'){ //check is req is made in server or browser
+        const { data } = await axios.get( //format: http://SERVICENAME.NAMESPACE.cluster.local/*
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',{
             headers:{
-                Host:'ticketing.dev'
+                Host:'ticketing.dev' //required to tell ingress about the domain
             }
         });
         return data;
-    } else {
+    } else {//since this is in the browser no need to add domain
         const { data } = await axios.get('/api/users/currentUser');
         return data;
     }
