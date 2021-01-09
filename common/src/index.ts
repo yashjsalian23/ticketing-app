d80e729929a30541631ed9c202597587d9ce97f8
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-09 22:40:19 +0530

    added errors and middleware to common file

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,2 +1,11 @@
-let i = "tjn";
-console.log(i);
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
