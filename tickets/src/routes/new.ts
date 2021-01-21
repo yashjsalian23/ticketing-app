e84ee64100ffb6480a456716eeabf2f20953c1f0
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-21 22:08:57 +0530

    added logic to create new ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,16 +1,27 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth,validateRequest } from '@ticketnext/common'
+import { requireAuth,validateRequest } from '@ticketnext/common';
+
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.post('/api/tickets',requireAuth,[
-    body('title').not().isEmpty().withMessage('Enter valid title'),
+    body('title').not().isEmpty().withMessage('Enter valid Title'),
     body('price').isInt({gt:0}).withMessage('Enter valid Price')
 ], validateRequest,
-(req:Request, res:Response) => {
-    res.status(200).send("hi");
-})
+async (req:Request, res:Response) => {
+    let { title, price } = req.body;
+
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    });
+
+    await ticket.save();
+    res.status(201).send(ticket);
+});
 
 export { router as createTicketRouter };
 
