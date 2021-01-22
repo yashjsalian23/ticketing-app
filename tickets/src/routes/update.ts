b78d0e4481c1b5404f578505856afffc783084fb
yashjsalian23 <yashjsalian23@gmail.com>
2021-01-22 12:07:56 +0530

    added logic to check unauthorized error

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,8 +8,9 @@ const router = express.Router();
 router.put('/api/tickets/:id', requireAuth, async (req:Request, res:Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
-    if(!ticket){throw new NotFoundError();}
+    if(!ticket){throw new NotFoundError();} // if ticket dosnt exist send 404
+    if(ticket.userId !== req.currentUser!.id){throw new NotAuthorizedError();} //if any other user is updating send 401
     res.send(ticket);
-})
+});
 
 export { router as updateTicketRouter };
